bb379021dc50450ea7a338a16eb0198236a2abe0
ditoma95 <devgtfod@gmail.com>
2023-12-16 13:58:40 +0000

    fist

---
+++ src/test/java/com/ifntil3jee/jee/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifntil3jee.jee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
