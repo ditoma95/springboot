78d625719b174c94efa548d9796ebb1206799159
ditoma95 <devgtfod@gmail.com>
2024-01-05 16:54:21 +0000

    template

---
+++ src/main/frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../resources/templates/**/*.{html,js}"], // it will be explained later
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
